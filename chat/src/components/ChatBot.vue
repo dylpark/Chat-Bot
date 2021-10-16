767cf6b1c7917987215ef6481ef0c13aaffb9868
Dylan Park <64296934+dylpark@users.noreply.github.com>
2021-10-16 11:37:34 +1000

    Update ChatBot.vue

--- chat/src/components/ChatBot.vue
+++ chat/src/components/ChatBot.vue
@@ -1,17 +1,109 @@
 <template>
-  <v-container>
-    <div class="chat-bot">
 
-    <v-btn class="open-button"
-    type="button" 
-    @click="open"> Open Chat
-    </v-btn>
+  <div>
 
-    <v-btn class="close-button" 
-    type="button" 
-    @click="close">Close Chat
-    </v-btn>
-      
+    <v-card
+      class="d-flex flex-row-reverse"
+      :color="$vuetify.theme.dark ? 'grey darken-3' : 'grey lighten-4'"
+      flat
+      tile
+    >
+      <v-card
+        v-for="n in 3"
+        :key="n"
+        class="pa-2"
+        outlined
+        tile
+      >
+        Flex item {{ n }}
+      <div>
+
+      <div class="chat-bot">
+
+      <v-btn class="open-button"
+        elevation="2"
+        outlined
+        depressed
+        small
+        x-large
+        type="button" 
+        @click="open">Open
+      </v-btn>
+
+      <v-btn class="close-button"
+        elevation="2"
+        outlined
+        depressed
+        small
+        x-large
+        type="button" 
+        @click="close">Close Chat
+      </v-btn>
+
+      <div v-if="openChat" class="chat-bot-list-container">
+        <ul class="chat-bot-list">
+          <li class="message"
+            v-for="(message, index) in messages"
+            :key="index"
+            :class="message.author"
+          >
+          <p>
+            <span>{{ message.text }}</span>
+          </p>
+          </li>
+        </ul>
+      </div>
+
+      <div class="chat-inputs">
+        <v-text-field 
+        type="text" 
+        v-model="message" 
+        @keyup.enter="sendMessage"
+        label="Main input"
+        :rules="rules"
+        hide-details="auto">
+        </v-text-field>
+
+        <v-btn @click="sendMessage">Send</v-btn>
+      </div>
+
+      </div>
+    </div>
+  </v-card>
+  </v-card>
+  </div>
+
+</template>
+
+
+<!----------------------------------------------------------------
+<template>
+<v-container>
+  <v-row justify="space-between">
+    
+    <v-col md="3">
+      <div class="chat-bot">
+
+      <v-btn class="open-button"
+        elevation="2"
+        outlined
+        depressed
+        small
+        x-large
+        type="button" 
+        @click="open">Open
+      </v-btn>
+
+      <v-btn class="close-button"
+        elevation="2"
+        outlined
+        depressed
+        small
+        x-large
+        type="button" 
+        @click="close">Close Chat
+      </v-btn>
+
       <div v-if="openChat" class="chat-bot-list-container">
 
         <ul class="chat-bot-list">
@@ -40,16 +132,25 @@
         :rules="rules"
         hide-details="auto">
         </v-text-field>
-        
-        <v-btn @click="sendMessage">Send</v-btn>
+      
+      </div>
 
       </div>
-  </div>
-  </v-container>
+
+    <v-row justify="space-around">
+      <v-col md="3">
+              <v-btn @click="sendMessage">Send</v-btn>
+      </v-col>
+    </v-row>
+      
+    </v-col>
+
+  </v-row> 
+</v-container>
 </template>
+---------------------------------------------------------------->
 
 <script>
-
 export default {
   name: 'ChatBot',
   data() {
@@ -57,7 +158,12 @@ export default {
       openChat: false,
       connection: null,
       message: '',
-      messages: []
+      messages: [],
+      alignments: [
+        'start',
+        'center',
+        'end',
+      ],
     }
 
   },
