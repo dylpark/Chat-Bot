50da54945fe19e534462fee99d338d6b5147110e
Dylan Park <64296934+dylpark@users.noreply.github.com>
2021-10-16 13:25:28 +1000

    Working Correctly

--- chat/src/components/ChatBot.vue
+++ chat/src/components/ChatBot.vue
@@ -1,7 +1,6 @@
 <template>
 
-  <div>
-
+  <v-container>
     <v-card
       class="d-flex flex-row-reverse"
       :color="$vuetify.theme.dark ? 'grey darken-3' : 'grey lighten-4'"
@@ -9,14 +8,13 @@
       tile
     >
       <v-card
-        v-for="n in 3"
+        v-for="n in 1"
         :key="n"
         class="pa-2"
         outlined
         tile
       >
         Flex item {{ n }}
-      <div>
 
       <div class="chat-bot">
 
@@ -24,7 +22,6 @@
         elevation="2"
         outlined
         depressed
-        small
         x-large
         type="button" 
         @click="open">Open
@@ -34,7 +31,6 @@
         elevation="2"
         outlined
         depressed
-        small
         x-large
         type="button" 
         @click="close">Close Chat
@@ -48,6 +44,7 @@
             :class="message.author"
           >
           <p>
+            <span>{{ message.author }}</span>
             <span>{{ message.text }}</span>
           </p>
           </li>
@@ -68,87 +65,13 @@
       </div>
 
       </div>
-    </div>
+
   </v-card>
   </v-card>
-  </div>
+
+  </v-container>
 
 </template>
-
-
-<!----------------------------------------------------------------
-<template>
-<v-container>
-  <v-row justify="space-between">
-    
-    <v-col md="3">
-      <div class="chat-bot">
-
-      <v-btn class="open-button"
-        elevation="2"
-        outlined
-        depressed
-        small
-        x-large
-        type="button" 
-        @click="open">Open
-      </v-btn>
-
-      <v-btn class="close-button"
-        elevation="2"
-        outlined
-        depressed
-        small
-        x-large
-        type="button" 
-        @click="close">Close Chat
-      </v-btn>
-
-      <div v-if="openChat" class="chat-bot-list-container">
-
-        <ul class="chat-bot-list">
-          <li class="message"
-            v-for="(message, index) in messages"
-            :key="index"
-            :class="message.author"
-          >
-
-          <p>
-            <span>{{ message.text }}</span>
-          </p>
-
-          </li>
-        </ul>
-
-      </div>
-
-      <div class="chat-inputs">
-        
-        <v-text-field 
-        type="text" 
-        v-model="message" 
-        @keyup.enter="sendMessage"
-        label="Main input"
-        :rules="rules"
-        hide-details="auto">
-        </v-text-field>
-      
-      </div>
-
-      </div>
-
-    <v-row justify="space-around">
-      <v-col md="3">
-              <v-btn @click="sendMessage">Send</v-btn>
-      </v-col>
-    </v-row>
-      
-    </v-col>
-
-  </v-row> 
-</v-container>
-</template>
----------------------------------------------------------------->
 
 <script>
 export default {
@@ -169,45 +92,31 @@ export default {
   },
   mounted() {
     this.connection = new WebSocket('ws://localhost:3000')
+    this.connection.onmessage = (event) => {
+      console.log("Got this back: " + event.data)
+      this.messages.push({
+        text: event.data,
+        author: 'bot'
+        })
+
+    }
   },
+
   methods: {
+
     open() {
       this.openChat = true;
-          this.connection.send('New Connection From Client')
+      this.connection.send('User opened chat')
     },
-    sendMessage() {
-      let message = this.message
-      let botMessage = this.connection.send(`Bot: ${message}`)
 
+    sendMessage() {
+      this.connection.send(this.message)
       this.messages.push({
-        text: message,
+        text: this.message,
         author: 'client'
       })
-
-      this.connection.send(message)
-
-      message = ''
-
-      .then(() => {
-        this.message.push({
-          text: botMessage,
-          author: 'bot'
-        })
-      })
-
-      // this.connection.send(this.currentMessage)
-      // {
-      //   text: this.currentMessage,
-      //   author: 'Client'
-      // })
-
-      // this.connection.send(this.message)
-      // messages.push(this.message);
-      // {
-      //   text: 'Response from Bot',
-      //   author: 'Bot'
-      // })
     },
+
     close() {
       this.openChat = false;
       this.connection.send('Chat Ended')
