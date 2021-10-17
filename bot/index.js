4e8d14611e35022533e66476b41bf0a50e3f3574
Dylan Park <64296934+dylpark@users.noreply.github.com>
2021-10-17 13:03:34 +1000

    Formatting

--- bot/index.js
+++ bot/index.js
@@ -1,40 +1,33 @@
 //
 //
 
-const { WebSocketServer } = require('ws');
+const { WebSocketServer } = require("ws");
 const wss = new WebSocketServer({ port: 3000 });
 
 let messageCount = 0;
 let name = "";
 
-wss.on('connection', ws => {
-  console.log("New connection")
+wss.on("connection", (ws) => {
+  console.log("New connection");
 
-  ws.on('message',(message) => {
-    console.log("Count", messageCount)
-    console.log('received: %s', message);
-    
+  ws.on("message", (message) => {
+    console.log("Count", messageCount);
+    console.log("received: %s", message);
+
     if (messageCount === 0) {
-      ws.send('Whats your name?')
-
+      ws.send("Whats your name?");
     } else if (messageCount === 1) {
       name = message;
-      ws.send(`How can I help you ${message}?`)
-
-    } else if (message.includes('bye' || 'goodbye')) {
+      ws.send(`How can I help you ${message}?`);
+    } else if (message.includes("bye" || "goodbye")) {
       ws.send(`See ya ${name}`);
-
     } else {
       ws.send(`${message}`);
     }
     messageCount++;
-
   });
 
-  ws.on('close', () => {
-    console.log('Disconnected');
+  ws.on("close", () => {
+    console.log("Disconnected");
   });
-  
 });
-
-
