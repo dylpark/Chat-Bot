9c42fb071f56a2ca09cbc11d5c46bf7a8622f35e
Dylan Park <dylpark@protonmail.com>
2021-10-19 11:29:18 +1000

    Update index.js

--- bot/index.js
+++ bot/index.js
@@ -10,17 +10,22 @@ let name = "";
 wss.on("connection", (ws) => {
   console.log("New connection");
 
+  ws.send("You've connected!")
+
   ws.on("message", (message) => {
     console.log("Count", messageCount);
     console.log("received: %s", message);
 
     if (messageCount === 0) {
       ws.send("Whats your name?");
+
     } else if (messageCount === 1) {
       name = message;
       ws.send(`How can I help you ${message}?`);
+
     } else if (message.includes("bye" || "goodbye")) {
       ws.send(`See ya ${name}`);
+      
     } else {
       ws.send(`${message}`);
     }
@@ -29,5 +34,7 @@ wss.on("connection", (ws) => {
 
   ws.on("close", () => {
     console.log("Disconnected");
+    messageCount = 0;
+    name = "";
   });
 });
